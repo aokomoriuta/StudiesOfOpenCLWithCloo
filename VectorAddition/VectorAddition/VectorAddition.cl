c13df55c5d85f68ad1dda26ed4725a76f457e206
aokomoriuta_lab2 <internal@enmps.net>
2012-08-10 19:48:45 +0900

    ベクトル扱いと複数要素を追加

--- VectorAddition/VectorAddition/VectorAddition.cl
+++ VectorAddition/VectorAddition/VectorAddition.cl
@@ -4,23 +4,91 @@
 
 //! REAL is provided by compiler option
 typedef REAL Real;
+typedef REALV RealV;
 
-
-//! Add each vector element
+//! Add one element per one work-item
 /*!
-	\param answer vector which answer is stored to
+	\param result vector which result is stored to
 	\param left adding vector
 	\param right added vector
 	\param C coefficient for added vector
 */
-__kernel void AddEachVector(
-	__global Real* answer,
-	__global const Real* left,
-	__global const Real* right)
+__kernel void AddOneElement(
+	__global Real* result,
+	const __global const Real* left,
+	const __global const Real* right)
 {
 	// get element index
 	int i = get_global_id(0);
 
 	// add each element
-	answer[i] = left[i] + right[i];
+	result[i] = left[i] + right[i];
+}
+
+
+//! Add one element as vector per one work-item
+/*!
+	\param result vector which result is stored to
+	\param left adding vector
+	\param right added vector
+	\param C coefficient for added vector
+*/
+__kernel void AddOneVector(
+	__global Real* result,
+	const __global const Real* left,
+	const __global const Real* right)
+{
+	// get element index
+	int i = get_global_id(0);
+
+	// load as vector
+	RealV leftVector =  VLOADN(i, left);
+	RealV rightVector = VLOADN(i, right);
+
+	// add as vector
+	RealV resultVector = leftVector + rightVector;
+
+	// store result
+	VSTOREN(resultVector, i, result);
+}
+
+
+//! Add more than one element per one work-item
+/*!
+	\param result vector which result is stored to
+	\param left adding vector
+	\param right added vector
+	\param C coefficient for added vector
+*/
+__kernel void AddMoreElement(
+	__global Real* result,
+	const __global const Real* left,
+	const __global const Real* right)
+{
+	// get element index
+	int i = get_global_id(0);
+
+	//// for all target element by this work-item
+	//for(int j = 0; j < COUNT_PER_WORKITEM; j++)
+	//{
+	//	// add element
+	//	result[i+j] = left[i+j] + right[i+j];
+	//}
+#define ADDONE(j) result[COUNT_PER_WORKITEM*i+j]=left[COUNT_PER_WORKITEM*i+j]+right[COUNT_PER_WORKITEM*i+j];
+	ADDONE( 0)
+	ADDONE( 1)
+	ADDONE( 2)
+	ADDONE( 3)
+	ADDONE( 4)
+	ADDONE( 5)
+	ADDONE( 6)
+	ADDONE( 7)
+	ADDONE( 8)
+	ADDONE( 9)
+	ADDONE(10)
+	ADDONE(11)
+	ADDONE(12)
+	ADDONE(13)
+	ADDONE(14)
+	ADDONE(15)
 }
--- VectorAddition/VectorAddition/VectorAdditionMain.cs
+++ VectorAddition/VectorAddition/VectorAdditionMain.cs
@@ -16,7 +16,22 @@ namespace LWisteria.StudiesOfOpenTKWithCloo.VectorAddition
 		/// <summary>
 		/// 要素数
 		/// </summary>
-		const int COUNT = 20000000;
+		const int COUNT = 1024 * 64 * 16;
+
+		/// <summary>
+		/// ベクトルとして扱う場合の要素数
+		/// </summary>
+		const int VCOUNT = 16;
+
+		/// <summary>
+		/// 1ワークアイテムが計算する要素数
+		/// </summary>
+		const int COUNT_PER_WORKITEM = 16;
+
+		/// <summary>
+		/// 検算用ベクトル
+		/// </summary>
+		static Real[] answer;
 
 		/// <summary>
 		/// コマンドキュー群
@@ -37,14 +52,24 @@ namespace LWisteria.StudiesOfOpenTKWithCloo.VectorAddition
 		/// <summary>
 		/// 結果すべてのバッファー
 		/// </summary>
-		static ComputeBuffer<Real> bufferAnswer;
+		static ComputeBuffer<Real> bufferResult;
 		#endregion
 
 		#region カーネル
 		/// <summary>
-		/// 各要素をそれぞれ足すカーネル
+		/// 各要素を1つずつ足すカーネル
 		/// </summary>
-		static ComputeKernel addEachVector;
+		static ComputeKernel addOneElement;
+
+		/// <summary>
+		/// ベクトルとして1つずつ足すカーネル
+		/// </summary>
+		static ComputeKernel addOneVector;
+
+		/// <summary>
+		/// 各要素を複数ずつ足すカーネル
+		/// </summary>
+		static ComputeKernel addMoreElement;
 		#endregion
 
 		/// <summary>
@@ -58,13 +83,18 @@ namespace LWisteria.StudiesOfOpenTKWithCloo.VectorAddition
 			// 計算対象および結果を格納するベクトルを作成
 			var left = new Real[COUNT];
 			var right = new Real[COUNT];
-			var answer = new Real[COUNT];
+			var result = new Real[COUNT];
+
+			// 検算用ベクトル
+			answer = new Real[COUNT];
 
 			// 計算対象のデータを作成
 			for(int i = 0; i < COUNT; i++)
 			{
-				left[i] = i / COUNT;
-				right[i] = i * i / COUNT;
+				left[i] = (double)i / COUNT;
+				right[i] = (double)i * i / COUNT;
+
+				answer[i] = left[i] + right[i];
 			}
 
 			// OpenCLの使用準備
@@ -73,11 +103,15 @@ namespace LWisteria.StudiesOfOpenTKWithCloo.VectorAddition
 			// 実行開始
 			Console.WriteLine("== 実行速度計測開始 ==");
 			Console.WriteLine("要素数：{0}", COUNT);
+			Action<string, bool, Action> showResult = (name, useGpu, action) =>
+				Console.WriteLine("{0}: {1,9}", name, ProcessingTime(action, useGpu, result));
 
 			// 各方法で実行して結果を表示
-			Console.WriteLine("単一CPU: {0,8}[ms]", ProcessingTime(() => SingleCpuAddition(answer, left, right)));
-			Console.WriteLine("複数CPU: {0,8}[ms]", ProcessingTime(() => ParallelCpuAddition(answer, left, right)));
-			Console.WriteLine("単一GPU: {0,8}[ms]", ProcessingTime(() => SingleGpuAddition(answer, left, right)));
+			showResult("        単一CPU", false, () => SingleCpuAddition(result, left, right));
+			showResult("        複数CPU", false, () => ParallelCpuAddition(result, left, right));
+			showResult("GPU（各要素）  ", true, () => SingleGpuAdditionOneElement(result, left, right));
+			showResult("GPU（ベクトル）", true, () => SingleGpuAdditionOneVector(result, left, right));
+			showResult("GPU（複数要素）", true, () => SingleGpuAdditionMoreElement(result, left, right));
 
 			// 成功で終了
 			return System.Environment.ExitCode;
@@ -121,7 +155,8 @@ namespace LWisteria.StudiesOfOpenTKWithCloo.VectorAddition
 				string realString = ((typeof(Real) == typeof(Double)) ? "double" : "float");
 
 				program.Build(devices,
-					"-D REAL=" + realString + " -Werror", null, IntPtr.Zero);
+					string.Format(" -D REAL={0} -D REALV={0}{1} -D VLOADN=vload{1} -D VSTOREN=vstore{1} -D COUNT_PER_WORKITEM={2} -Werror", realString, VCOUNT, COUNT_PER_WORKITEM),
+					null, IntPtr.Zero);
 			}
 			// 失敗したら
 			catch(BuildProgramFailureComputeException ex)
@@ -131,12 +166,14 @@ namespace LWisteria.StudiesOfOpenTKWithCloo.VectorAddition
 			}
 
 			// カーネルを作成
-			addEachVector = program.CreateKernel("AddEachVector");
+			addOneElement = program.CreateKernel("AddOneElement");
+			addOneVector = program.CreateKernel("AddOneVector");
+			addMoreElement = program.CreateKernel("AddMoreElement");
 
 			// バッファーを作成
 			bufferLeft = new ComputeBuffer<Real>(context, ComputeMemoryFlags.ReadOnly, COUNT);
 			bufferRight = new ComputeBuffer<Real>(context, ComputeMemoryFlags.ReadOnly, COUNT);
-			bufferAnswer = new ComputeBuffer<Real>(context, ComputeMemoryFlags.WriteOnly, COUNT);
+			bufferResult = new ComputeBuffer<Real>(context, ComputeMemoryFlags.WriteOnly, COUNT);
 		}
 
 		/// <summary>
@@ -144,10 +181,29 @@ namespace LWisteria.StudiesOfOpenTKWithCloo.VectorAddition
 		/// </summary>
 		/// <param name="action">計測対象の操作</param>
 		/// <returns>実行時間（ミリ秒）</returns>
-		static long ProcessingTime(Action action)
+		static long ProcessingTime(Action action, bool useGpu, Real[] result)
 		{
 			// ストップウォッチ作成
 			var stopwatch = new System.Diagnostics.Stopwatch();
+
+			// すべての結果を
+			for(int i = 0; i < COUNT; i++)
+			{
+				// 0に初期化
+				result[i] = 0;
+			}
+
+			// GPUなら
+			if(useGpu)
+			{
+				// 全てのデバイスで
+				foreach(var queue in queues)
+				{
+					// 結果バッファーに初期状態を転送
+					queue.WriteToBuffer(result, bufferResult, true, null);
+				}
+			}
+
 
 			// 計測開始
 			stopwatch.Restart();
@@ -155,49 +211,64 @@ namespace LWisteria.StudiesOfOpenTKWithCloo.VectorAddition
 			// 操作を実行
 			action();
 
+			// 計測終了
+			stopwatch.Stop();
+
+
+			// すべての結果を
+			for(int i = 0; i < COUNT; i++)
+			{
+				// 検算して違えば
+				if(result[i] != answer[i])
+				{
+					// 出力
+					Console.WriteLine("{0,8}: {1,5:f} vs {2,5:f}", i, result[i], answer[i]); 
+				}
+			}
+
 			// 実行時間を返す
-			return stopwatch.ElapsedMilliseconds;
+			return stopwatch.ElapsedTicks;
 		}
 
 		/// <summary>
 		/// CPUで並列させずに加算を実行する
 		/// </summary>
-		/// <param name="answer">結果を格納する対象</param>
+		/// <param name="result">結果を格納する対象</param>
 		/// <param name="left">計算対象1</param>
 		/// <param name="right">計算対象2</param>
-		static void SingleCpuAddition(Real[] answer, Real[] left, Real[] right)
+		static void SingleCpuAddition(Real[] result, Real[] left, Real[] right)
 		{
 			// 全要素を順に
 			for(int i = 0; i < COUNT; i++)
 			{
 				// 足す
-				answer[i] = left[i] + right[i];
+				result[i] = left[i] + right[i];
 			}
 		}
 
 		/// <summary>
 		/// CPUで並列させて加算を実行する
 		/// </summary>
-		/// <param name="answer">結果を格納する対象</param>
+		/// <param name="result">結果を格納する対象</param>
 		/// <param name="left">計算対象1</param>
 		/// <param name="right">計算対象2</param>
-		static void ParallelCpuAddition(Real[] answer, Real[] left, Real[] right)
+		static void ParallelCpuAddition(Real[] result, Real[] left, Real[] right)
 		{
 			// 全要素を並列で
 			System.Threading.Tasks.Parallel.For(0, COUNT, (i) =>
 			{
 				// 足す
-				answer[i] = left[i] + right[i];
+				result[i] = left[i] + right[i];
 			});
 		}
 
 		/// <summary>
-		/// GPUを1つ使って加算を実行する
+		/// GPUを1つ使って1つずつ加算を実行する
 		/// </summary>
-		/// <param name="answer">結果を格納する対象</param>
+		/// <param name="result">結果を格納する対象</param>
 		/// <param name="left">計算対象1</param>
 		/// <param name="right">計算対象2</param>
-		static void SingleGpuAddition(Real[] answer, Real[] left, Real[] right)
+		static void SingleGpuAdditionOneElement(Real[] result, Real[] left, Real[] right)
 		{
 			// 使用するキューを設定
 			var queue = queues[0];
@@ -210,15 +281,81 @@ namespace LWisteria.StudiesOfOpenTKWithCloo.VectorAddition
 			//  # 結果を格納するベクトル
 			//  # 計算対象のベクトル1
 			//  # 計算対象のベクトル2
-			addEachVector.SetMemoryArgument(0, bufferAnswer);
-			addEachVector.SetMemoryArgument(1, bufferLeft);
-			addEachVector.SetMemoryArgument(2, bufferRight);
+			addOneElement.SetMemoryArgument(0, bufferResult);
+			addOneElement.SetMemoryArgument(1, bufferLeft);
+			addOneElement.SetMemoryArgument(2, bufferRight);
 
 			// 計算を実行
-			queue.Execute(addEachVector, null, new long[] { COUNT }, null, null);
+			queue.Execute(addOneElement, null, new long[] { COUNT }, null, null);
 
 			// 結果を読み込み
-			queue.ReadFromBuffer(bufferAnswer, ref answer, false, null);
+			queue.ReadFromBuffer(bufferResult, ref result, false, null);
+
+			// 終了まで待機
+			queue.Finish();
+		}
+
+		/// <summary>
+		/// GPUを1つ使ってベクトルとして加算を実行する
+		/// </summary>
+		/// <param name="result">結果を格納する対象</param>
+		/// <param name="left">計算対象1</param>
+		/// <param name="right">計算対象2</param>
+		static void SingleGpuAdditionOneVector(Real[] result, Real[] left, Real[] right)
+		{
+			// 使用するキューを設定
+			var queue = queues[0];
+
+			// 計算対象のデータを転送
+			queue.WriteToBuffer(left, bufferLeft, false, null);
+			queue.WriteToBuffer(right, bufferRight, false, null);
+
+			// 引数を設定
+			//  # 結果を格納するベクトル
+			//  # 計算対象のベクトル1
+			//  # 計算対象のベクトル2
+			addOneVector.SetMemoryArgument(0, bufferResult);
+			addOneVector.SetMemoryArgument(1, bufferLeft);
+			addOneVector.SetMemoryArgument(2, bufferRight);
+
+			// 計算を実行
+			queue.Execute(addOneVector, null, new long[] { COUNT / VCOUNT }, null, null);
+
+			// 結果を読み込み
+			queue.ReadFromBuffer(bufferResult, ref result, false, null);
+
+			// 終了まで待機
+			queue.Finish();
+		}
+
+		/// <summary>
+		/// GPUを1つ使って複数ずつ加算を実行する
+		/// </summary>
+		/// <param name="result">結果を格納する対象</param>
+		/// <param name="left">計算対象1</param>
+		/// <param name="right">計算対象2</param>
+		static void SingleGpuAdditionMoreElement(Real[] result, Real[] left, Real[] right)
+		{
+			// 使用するキューを設定
+			var queue = queues[0];
+
+			// 計算対象のデータを転送
+			queue.WriteToBuffer(left, bufferLeft, false, null);
+			queue.WriteToBuffer(right, bufferRight, false, null);
+
+			// 引数を設定
+			//  # 結果を格納するベクトル
+			//  # 計算対象のベクトル1
+			//  # 計算対象のベクトル2
+			addMoreElement.SetMemoryArgument(0, bufferResult);
+			addMoreElement.SetMemoryArgument(1, bufferLeft);
+			addMoreElement.SetMemoryArgument(2, bufferRight);
+
+			// 計算を実行
+			queue.Execute(addMoreElement, null, new long[] { COUNT/COUNT_PER_WORKITEM }, null, null);
+
+			// 結果を読み込み
+			queue.ReadFromBuffer(bufferResult, ref result, false, null);
 
 			// 終了まで待機
 			queue.Finish();
